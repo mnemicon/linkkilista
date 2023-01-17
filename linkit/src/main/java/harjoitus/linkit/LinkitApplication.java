05f2aba15a6eaad012532158bb19f4f7244ab59c
Maestro <mica@morph.fi>
2023-01-17 16:03:18 +0200

    Create spring project structure

---
+++ linkit/src/main/java/harjoitus/linkit/LinkitApplication.java
@@ -0,0 +1,13 @@
+package harjoitus.linkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkitApplication.class, args);
+	}
+
+}
